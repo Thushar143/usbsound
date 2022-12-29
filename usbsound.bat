37042ff133eaca2535e3f97f95e8c4cc23f87e2e
Thushar143 <41180801+Thushar143@users.noreply.github.com>
2022-12-29 22:11:11 +0530

    Update usbsound.bat

--- usbsound.bat
+++ usbsound.bat
@@ -2,22 +2,21 @@ copy C:\usbsound\eject.wav C:\Windows\Media\
 copy C:\usbsound\insert.wav C:\Windows\Media\
 mmsys.cpl
 timeout 3 > null
-set command = (New-Object -ComObject WScript.Shell)
-powershell -c %command%.SendKeys('{TAB 3}')
-powershell -c %command%.SendKeys('{RIGHT 2}')
-powershell -c %command%.SendKeys('{TAB 4}')
-powershell -c %command%.SendKeys('{DOWN 8}')
-powershell -c %command%.SendKeys('{TAB 4}')
-powershell -c %command%.SendKeys('{ENTER}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{TAB 3}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{RIGHT 2}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{TAB 4}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{DOWN 8}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{TAB 4}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{ENTER}')
 insert
 timeout 2 > null
-powershell -c %command%.SendKeys('{TAB 3}')
-powershell -c %command%.SendKeys('{ENTER}')
-powershell -c %command%.SendKeys('{TAB 3}')
-powershell -c %command%.SendKeys('{DOWN}')
-powershell -c %command%.SendKeys('{TAB 4}')
-powershell -c %command%.SendKeys('{ENTER}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{TAB 3}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{ENTER}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{TAB 3}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{DOWN}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{TAB 4}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{ENTER}')
 eject
 timeout 2 > null
-powershell -c %command%.SendKeys('{TAB 3}')
-powershell -c %command%.SendKeys('{ENTER 2}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{TAB 3}')
+powershell -c (New-Object -ComObject WScript.Shell).SendKeys('{ENTER 2}')
