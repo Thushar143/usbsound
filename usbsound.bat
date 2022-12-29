4d98532f4bf671f1bd8628dd04207506d1eb0f4c
Thushar143 <41180801+Thushar143@users.noreply.github.com>
2022-12-29 22:03:35 +0530

    Update usbsound.bat

--- usbsound.bat
+++ usbsound.bat
@@ -1,2 +1,23 @@
 copy C:\usbsound\eject.wav C:\Windows\Media\
 copy C:\usbsound\insert.wav C:\Windows\Media\
+mmsys.cpl
+timeout 3 > null
+set command = (New-Object -ComObject WScript.Shell)
+powershell -c %commmand%.SendKeys('{TAB 3}')
+powershell -c %command%.SendKeys('{RIGHT 2}')
+powershell -c %commmand%.SendKeys('{TAB 4}')
+powershell -c %command%.SendKeys('{DOWN 8}')
+powershell -c %command%.SendKeys('{TAB 4}')
+powershell -c %command%.SendKeys('{ENTER}')
+insert
+timeout 2 > null
+powershell -c %command%.SendKeys('{TAB 3}')
+powershell -c %command%.SendKeys('{ENTER}')
+powershell -c %command%.SendKeys('{TAB 3}')
+powershell -c %command%.SendKeys('{DOWN}')
+powershell -c %command%.SendKeys('{TAB 4}')
+powershell -c %command%.SendKeys('{ENTER}')
+eject
+timeout 2 > null
+powershell -c %command%.SendKeys('{TAB 3}')
+powershell -c %command%.SendKeys('{ENTER 2}')
